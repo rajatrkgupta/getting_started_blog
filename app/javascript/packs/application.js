20626714580ac054857a1ad21d7a78dc86c1089a
Rajat Kumar Gupta <rajat205130@gmail.com>
2021-10-21 12:53:57 +0530

    Integrate Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("bootstrap")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../stylesheets/application";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
