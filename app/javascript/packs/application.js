52c5300085c406919fe2463733f4cddcd9cfb30c
Rajat <rajat205130@gmail.com>
2022-03-28 01:07:45 +0530

    8.1 yarn add cropperjs package configuration with temp file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
+import Cropper from 'cropperjs'
+window.Cropper = Cropper
 
 Rails.start()
 Turbolinks.start()
