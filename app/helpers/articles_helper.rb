e80e950cb01b58d58eb056b7f2c0fe5e2a6bc5c4
Rajat <rajat205130@gmail.com>
2022-03-27 19:22:11 +0530

    Add some UI Fixes

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def show_articles_index_card_image(article)
+    image = article.image.persisted? ? article.image : "default_image.png"
+
+    link_to article_path(article) do
+      image_tag image, class: "card-img-top", alt: article.title
+    end
+  end
 end
